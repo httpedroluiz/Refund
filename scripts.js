4f60e94ed0a5109ab92c1db4484be96b15e13b48
Pedro Luiz <pedroluizmizael@gmail.com>
2024-08-07 22:37:08 -0300

    format amount script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "")
+
+  value = Number(value) / 100
+  
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
+}
+
 
