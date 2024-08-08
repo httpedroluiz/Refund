a38641f13068062f5dd7def5f0db5b0a1722b888
Pedro Luiz <pedroluizmizael@gmail.com>
2024-08-07 22:43:26 -0300

    create expenseItem on li

--- scripts.js
+++ scripts.js
@@ -20,4 +20,28 @@ function formatCurrencyBRL(value) {
   return value
 }
 
+form.onsubmit = (event) => {
+  event.preventDefault()
 
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+    
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.")
+    console.log(error)
+  }
+}
