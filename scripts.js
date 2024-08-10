0f176fb49944bd7d7fa17101707a3d14b87fd88d
Pedro Luiz <pedroluizmizael@gmail.com>
2024-08-10 00:03:30 -0300

    update total function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
 const expensesQuantity = document.querySelector("aside header p span")
 
 amount.oninput = () => {
@@ -84,6 +85,31 @@ function UpdateTotals() {
     const items = expenseList.children
 
     expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    let total = 0
+
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount")
+
+      let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
+
+      value = parseFloat(value)
+
+      if (isNaN(value)) {
+        return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+      }
+
+      total += Number(value)
+    }
+
+    const symbolBRL = document.createElement("small")
+    symbolBRL.textContent = "R$"
+
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+    expensesTotal.innerHTML = ""
+
+    expensesTotal.append(symbolBRL, total)
 
   } catch (error) {
     console.log(error)
